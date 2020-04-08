0225786e6cf2ee648069fcc485529221fb3f819f
Kushwanth Gondi <36468390+KushwanthG@users.noreply.github.com>
2020-04-08 23:46:52 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,11 @@
+t=('4','6','6','9')
+my_list=[5,9,3]
+print(type(t))
+print(type(my_list))
+print(len(t))
+print(t.count('6'))
+print(t.index('6'))
+my_list[0]='New'
+print(my_list)
+#t[0]='s'\ wo'nt change in tuples.
+ 
